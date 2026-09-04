bb83add1ec5e9c473ce4244b15571b4e32526c12
eomarcoss <marcoscesarmarinhocunha@gmail.com>
2026-09-04 14:28:04 -0300

    added <suspense> and increased API timeout

--- front/src/app/seller/(navegation)/announce/page.tsx
+++ front/src/app/seller/(navegation)/announce/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { CreateAdForm } from "@/components/shared/CreateAdForm";
+import { Suspense } from "react";
 
 export default function AnnouncePage() {
   return (
-    <div>
+    <Suspense fallback={<div>Loading...</div>}>
       <CreateAdForm />
-    </div>
+    </Suspense>
   );
 }
